8197740c99d211454f9b13e636cefd54caa61732
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-05-29 20:46:18 +0600

    Add files via upload

---
+++ implement_stack_using_stl_list.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class mystack{
+    public:
+    list<int> l;
+
+    void push(int val){
+        l.push_back(val);
+    }
+    void pop(){
+        l.pop_back();
+    }
+    int top(){
+        return l.back();
+    }
+    int size(){
+        return l.size();
+    }
+    bool empty(){
+        return l.empty();
+    }
+};
+int main(){
+    mystack st;
+    int t;cin>>t;
+    while(t--){
+        int x;cin>>x;
+        st.push(x);
+
+    }
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+}
